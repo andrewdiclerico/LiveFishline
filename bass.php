637fba22ceb89fff1e2c18e2ca231c65ee03c683
Andrew DiClerico <diclerico.andrew@gmail.com>
2022-12-12 21:35:36 -0500

    select statement test

--- bass.php
+++ bass.php
@@ -222,7 +222,7 @@
             <?php 
 
                 include_once __DIR__ . '../db.php';
-                $configFile = __DIR__ . '..//models/dbconfig.ini';
+                $configFile = __DIR__ . '../models/dbconfig.ini';
                                 
                 $sql = "SELECT * FROM fishline.addfish WHERE species = 'Bass'";
                 $res = mysqli_query($con, $sql);
