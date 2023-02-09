b2fc0ed4cdc6fce99959e0a5ee1424ac92e0c362
Andrew DiClerico <diclerico.andrew@gmail.com>
2023-02-09 13:30:28 -0500

    documentation page added

---
+++ docsite/documentation.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fishline Documentation Site | Capstone 2022</title>
+</head>
+<style>
+    a:hover{
+        color: #00FFFF;
+    }
+    a:visited:hover{
+        color: #00FFFF;
+    }
+
+    a:visited{
+        color: black;
+    }
+
+    a{
+        color: black;
+    }
+
+    body{
+        background-color: #2C5EE0;
+    }
+
+</style>
+<body>
+
+    <div style="margin-left:10%; margin-right:10%; text-align:center; padding-top:10px; padding-bottom:10px; background-color: white;">
+
+        <div style="margin-left:10%; margin-right:10%; background-color:#E3DFDE;">
+            
+            <h1 style="text-align:center;">
+                FishLine Documentation Site | Capstone 2022
+            </h1>
+            
+            <br />
+            
+            <h2 style="text-align:center;">
+                <a href="https://www.google.com"><u>Fishline</u></a>
+            </h2>
+
+            <br />
+
+        </div>
+
+        <div style="margin-left:10%; margin-right:10%; background-color:#E3DFDE;">
+            
+            <p>Introduction of each of the team members and a detailed description of the responsibilities of each member.</p>
+            
+            <div style="text-align:center;">
+            
+                <p>Nathan Tandon</p>
+                <p>Berta M. Puello-Martinrez</p>
+                <p>Andrew DiClerico</p>
+            
+            </div>
+        
+        </div>
+
+        <div style="margin-left:10%; margin-right:10%; background-color:#E3DFDE;">
+        
+        <p>Your <b>Proposal</b> in PDF or HTML.</p>
+            
+            <br />
+            
+            <a href="downloadables/Proposal.pdf" download="FishlineProposal">Proposal</a>
+
+            <br />
+            <br />
+        
+        </div>
+
+        <br />
+
+        <div style="margin-left:10%; margin-right:10%; background-color:#E3DFDE;">
+        
+            <p>Your <b>Prototype</b> in PDF or HTML.</p>
+        
+            <br />
+        
+            <a href="https://www.figma.com/file/PVrSacJsYxkrywFulMFdIC/CapstoneNBA?node-id=0%3A1" >Prototype</a>
+
+            <br />
+            <br />
+        
+        </div>
+
+        <br />
+        
+        <div style="margin-left:10%; margin-right:10%; background-color:#E3DFDE;">
+            
+            <p>Your <b>Technical Design Document</b> in PDF or HTML.</p>
+            
+            <br />
+            
+            <a href="downloadables/Tech Design.pdf" download="FishlineTechDesign">Tech Design</a>
+
+            <br />
+            <br />
+        
+        </div>
+
+        <br />
+
+        <div style="margin-left:10%; margin-right:10%; background-color:#E3DFDE;">
+        
+            <p>Your <b>ERD</b> in PDF or HTML.</p>
+            
+            <br />
+            
+            <a href="downloadables/ERD.pdf" download="FishlineERD">ERD</a>
+
+            <br />
+            <br />
+        
+        </div>
+
+        <br />
+
+        <div style="margin-left:10%; margin-right:10%; background-color:grey;">
+            
+            <p><b>Screenshots</b> of your application.</p>
+            
+            <br />
+            
+            <a href="downloadables/SS.pdf" download="FishlineScreenshots">Screenshots</a>
+
+            <br />
+            <br />
+        
+        </div>
+        
+        <br />
+
+        <div style="margin-left:10%; margin-right:10%; background-color:#E3DFDE;">
+        
+            <p>Copy of <b>PowerPoint</b> presentation.</p>
+        
+            <br />
+        
+            <a href="https://docs.google.com/presentation/d/1gdqWWR65YulpxDh0GoWJAi9Dd5XI7AgQOb0vy453E5c/edit?usp=sharing">PowerPoint</a>
+
+            <br />
+            <br />
+        
+        </div>
+
+        <br />
+
+        <div style="margin-left:10%; margin-right:10%; background-color:grey;">
+            
+            <p>Zipped file of all <b>Source Code</b>.</p>
+            
+            <br />
+            
+            <a href="" download="FishlineProposal">Source Code</a>
+
+            <br />
+            <br />
+        
+        </div>
+        
+        <br />
+
+        <div style="margin-left:10%; margin-right:10%; background-color:#E3DFDE;">
+        
+            <p>Link to <b>GitHub</b> repository.</p>
+            
+            <br />
+            
+            <a href="https://github.com/bmpuellomartinez/Capstone2022/tree/main/Project2022">GitHub</a>
+
+            <br />
+            <br />
+        
+        </div>
+
+
+    </div>
+
+    
+</body>
+
+<footer style="height:50px; padding-top:10px; color:white; background-color:#2C5EE0;">
+    © Fish Line - 2022
+</footer>
+</html>
